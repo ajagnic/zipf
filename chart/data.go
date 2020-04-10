e1e26599d055dda7fd3b02e27111cceb220e23b2
Adrian Agnic <adrian.agnic@hotmail.com>
2020-04-10 13:38:38 -0400

    Test with go-echarts lib

--- chart/data.go
+++ chart/data.go
@@ -1,25 +1,37 @@
 package chart
 
 import (
-	"fmt"
+	"log"
+	"os"
 	"sort"
+
+	"github.com/go-echarts/go-echarts/charts"
 )
 
 func Render(ratiomap map[float64][]string) {
-	format(ratiomap)
+	words, ratios := format(ratiomap)
+	bar := charts.NewBar()
+	bar.AddXAxis(words).AddYAxis("% Total", ratios)
+	filep, err := os.Create("bar.html")
+	if err != nil {
+		log.Println(err)
+	}
+	bar.Render(filep)
 }
 
-func format(ratiomap map[float64][]string) {
+func format(ratiomap map[float64][]string) (sortedwords []string, sortedratios []float64) {
 	ratioslice := []float64{}
 	for k, _ := range ratiomap {
 		ratioslice = append(ratioslice, k)
 	}
 	sort.Float64s(ratioslice)
-	sortedwords := []string{}
+	sortedwords = []string{}
+	sortedratios = []float64{}
 	for i := len(ratioslice) - 1; i >= 0; i-- {
 		for _, word := range ratiomap[ratioslice[i]] {
 			sortedwords = append(sortedwords, word)
+			sortedratios = append(sortedratios, ratioslice[i])
 		}
 	}
-	fmt.Println(sortedwords)
+	return
 }
--- check/error.go
+++
@@ -1,9 +0,0 @@
-package check
-
-import "log"
-
-func Fatal(err error) {
-	if err != nil {
-		log.Fatalf("\nFatalError: %v", err)
-	}
-}
